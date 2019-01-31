f9353f3c2db293afb163275d9229a60db068c570
ifanbin <ifanbin@outlook.com>
2019-01-31 14:34:47 +0800

    Initial commit

---
+++ src/main/java/com/hzlgit/rbmall/HomeController.java
@@ -0,0 +1,14 @@
+package com.hzlgit.rbmall;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "Hello, 覆盆子商城！";
+    }
+}
---
+++ src/main/java/com/hzlgit/rbmall/RbmallApplication.java
@@ -0,0 +1,14 @@
+package com.hzlgit.rbmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbmallApplication.class, args);
+    }
+
+}
+
